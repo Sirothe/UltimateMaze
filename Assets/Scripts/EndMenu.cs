68dae52c4067323b5e0b6bc2cc0dcfa54d7690ae
Maciej Szymborski <58854544+Sirothe@users.noreply.github.com>
2022-12-04 18:23:51 +0100

    EndMenu button adjustment

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class EndMenu : MonoBehaviour
 {
     public void QuitGame()
     {
-        Application.Quit();
+        if (Application.isEditor)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        } else 
+        {
+            Application.Quit();
+        }
     }
 }
